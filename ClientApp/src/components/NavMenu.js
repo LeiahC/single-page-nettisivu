caa60b7889eb62c41a95ce46e32ea5148370d4ec
Emmi K <koivunen.emmi@gmail.com>
2018-10-10 11:28:55 +0300

    lisätty datan haku fetch-toiminnolla.

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,4 +1,6 @@
-﻿import React, { Component } from 'react';
+﻿// Sivustolla näkyvä sivupalkki
+
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
